81eba5de9027f28d266870b6b7064ef455130487
Lei Cai <lei.cai@oulu.fi>
2021-03-16 11:22:30 +0200

    update class DataExplorer

--- geospacelab/dataexplorer/__init__.py
+++ geospacelab/dataexplorer/__init__.py
@@ -1,3 +1,33 @@
 # import geospacelab.dataexplorer._init_variable as BaseVariable
 
 from geospacelab.dataexplorer._init_dataset import Dataset
+
+
+class DataExplorer(object):
+
+    def __init__(self):
+        self._datasets = {}
+
+    def __setitem__(self, key, value):
+        if issubclass(value.__class__, Dataset):
+            self._datasets[key] = value
+        else:
+            raise TypeError
+
+    def __getitem__(self, key):
+        return self._datasets[key]
+
+    def add_dataset(self, dataset=None, **kwargs):
+        dataset_key = kwargs.pop('dataset_key', '')
+        if dataset is None:
+            # create a new dataset
+            dataset = Dataset(**kwargs)
+        elif not issubclass(dataset.__class__, Dataset):
+            raise TypeError
+
+        if not bool(dataset_key):
+            dataset_key = dataset.label()
+
+        self.__setitem__(dataset_key, dataset)
+
+
