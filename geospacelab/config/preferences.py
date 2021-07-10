f6f7cc78e02a62cbe6f880e63a6cd34c4467deff
Lei Cai <joulecai@gmail.com>
2021-07-10 16:41:01 +0300

    Define the class DataHub

--- geospacelab/config/preferences.py
+++ geospacelab/config/preferences.py
@@ -1,4 +1,7 @@
 
+
+package_name = "geospacelab"
+
 datahub_data_root_dir = "/Users/lcai/01-Work/00-Data/"
 
 
--- geospacelab/datahub/__init__.py
+++ geospacelab/datahub/__init__.py
@@ -1,124 +1,127 @@
 # import geospacelab.datahub._init_variable as BaseVariable
 import copy
 import importlib
+import datetime
 
 import pathlib
 
 import geospacelab.toolbox.utilities.pyclass as pyclass
 import geospacelab.toolbox.utilities.pybasic as pybasic
-from geospacelab.datahub._init_dataset import Dataset
+import geospacelab.toolbox.utilities.pylogging as mylog
+from geospacelab.datahub._init_dataset import DatasetBase
+import geospacelab.config.preferences as pfr
 
 datahub_root_dir = pathlib.Path(__file__).parent.absolute()
 
 def example():
 
-    var_config_list = []    # List of the variable configurations
-
+    dt_fr = datetime.datetime.strptime('20210309' + '0000', '%Y%m%d%H%M')
+    dt_to = datetime.datetime.strptime('20210309' + '2400', '%Y%m%d%H%M')
     database_name = 'madrigal'
     facility_name = 'eiscat'
-    assign_options = {'site': 'TRO', 'experiment': 'unknown', 'pulse_code': 'unknown', 'antenna':'UHF'}
 
-    var_config = VariableConfig(source_keys=[database_name, facility_name], assign_options=assign_options)
+    config_d = DatasetConfiguration(source_labels=[database_name, facility_name],
+                                          site='TRO', antenna='UHF')
+    config_v = VariableConfiguration()
 
-    var_config_list.append(var_config.update(variable='n_e'))
-    var_config_list.append(var_config.update(variable='T_i'))
-
-
-class Datasets(object):
-    def __init__(self):
-        self._datasets ={}
-
-        self.datasets = []
-        self.hashes = []
-        self.keys = []
-
-    def __setitem__(self, key, value):
-        if issubclass(value.__class__, Dataset):
-            self._datasets[key] = value
-        else:
-            raise TypeError
-
-    def __getitem__(self, key):
-        return self._datasets[key]
-
-    def get(self, **kwargs):
-        assign_keys = kwargs.pop('assign_keys', [])
-        module_keys = ['datahub']
-        module_keys.extend(assign_keys)
-        module = importlib.import_module('.'.join(module_keys) + '__init__.py')
-        dataset = module.set_dataset(**kwargs.pop('options'))
-
-
-
-
-        dataset_key = pybasic.dict_key_tree_plain()
-        dataset_hash = pybasic.string_to_hash(dataset_key)
-        try:
-            ind_hash = self.hashes.index(dataset_hash)
-            dataset = self.datasets[ind_hash]
-        except ValueError:
-            dataset = Dataset(**config_dict)
-            self.datasets.append(dataset)
-            self.hashes.append(dataset_hash)
-            self.keys.append(dataset_key)
+    dh = DataHub(dt_fr, dt_to)
+    dataset_label = dh.add_dataset(config_d)
+    v1 = dh.add_variables(config_v.replace('n_e'))
+    v2 = dh.add_variables(config_v.replace('T_i'))
 
 
 class DataHub(object):
 
-    def __init__(self, dt_fr, dt_to, var_config_list=None):
-        self.variables = Variables()
-        self.datasets = Datasets()
+    def __init__(self, dt_fr, dt_to):
+        self.variables = {}
+        self.datasets = {}
 
         self.dt_fr = dt_fr
         self.dt_to = dt_to
 
-        if var_config_list is not None:
-            self.assign_variables(var_config_list=var_config_list)
-
-    def assign_variables(self, var_config_list=None):
-
-        for var_config in var_config_list:
-            dataset = self.datasets.get(**var_config)
-
-    def get_dataset(self, var_config):
-        def generate_dataset_hash(var_config):
-
-        dataset_key = generate_dataset_key(var_config)
-        self.datasets
-
-
-
-    def assign_variables(self, var_config_list):
-        for var_config in var_config_list:
-            dataset = self.get_dataset(var_config)
-
-
-
-
-    def add_dataset(self, dataset=None, **kwargs):
-        dataset_key = kwargs.pop('dataset_key', '')
-        if dataset is None:
-            # create a new dataset
-            dataset = Dataset(**kwargs)
-        elif not issubclass(dataset.__class__, Dataset):
+    def add_dataset(self, *args, config=None, **kwargs):
+        dataset = kwargs.pop('dataset', None)
+        if config is None:
+            if dataset is None:
+                config = DatasetConfiguration(**kwargs)
+            elif issubclass(dataset, DatasetBase):
+                raise TypeError
+        elif not isinstance(config, DatasetConfiguration):
             raise TypeError
 
-        if not bool(dataset_key):
-            dataset_key = dataset.label()
+        if config is not None:
+            module_keys = [pfr.package_name, 'datahub', 'sources']
+            module_keys.extend(config.source_labels)
+            module = importlib.import_module('.'.join(module_keys))
+            dataset = module.Dataset(config=config, **kwargs)
 
-        self.__setitem__(dataset_key, dataset)
+            if dataset in self.datasets.keys():
+                mylog.simpleinfo.warning('The same dataset has been added!')
+            else:
+                dataset.load()
+                ind = len(self.datasets.keys()) + 1
+                self.datasets[ind] = dataset
+
+        return dataset
+
+    def add_variable(self, config=None, **kwargs):
+        variable = kwargs.pop('variable', None)
+        dataset = kwargs.pop('dataset', None)
+        dataset_ind = kwargs.pop('dataset_ind', None)
+        if dataset is None:
+            # use the last added dataset
+            if dataset_ind is not None:
+                dataset = self.datasets[dataset_ind]
+            else:
+                dataset = self.datasets[len(self.datasets.keys())] # the latest added dataset
+        elif issubclass(dataset, DatasetBase):
+            if dataset not in self.datasets.values():
+                self.add_dataset(dataset=dataset)
+        else:
+            raise TypeError
+
+        if config is not None:
+            if issubclass(config, VariableConfiguration):
+                variable = dataset.set_variable(config=config)
+            else:
+                raise TypeError
+        elif variable is not None:
+            if issubclass(variable, VariableBase):
+                variable = variable
+            else:
+                raise TypeError
+        return variable
 
 
-class VariableConfig(object):
-    def __init__(self, source_keys=None, var_name='', assign_options=None):
+class DatasetConfiguration(object):
+    def __init__(self, **kwargs):
+        self.source_labels = kwargs.pop('source_labels', [])
+        self.set_attr(**kwargs)
 
-        self.source_keys = source_keys
-        self.var_name = var_name
-        self.assign_options = assign_options
+    def set_attr(self, **kwargs):
+        pyclass.set_object_attributes(self, **kwargs, append=True, logging=False)
 
-    def update(self, **kwargs):
-        pyclass.set_object_attributes(self, **kwargs, append=False, logging=False)
-        return copy.deepcopy(self)  # return a copied objective
+    def replace(self, **kwargs):
+        new_obj = DatasetConfiguration()
+        new_obj.__dict__.update(self.__dict__)
+        pyclass.set_object_attributes(new_obj, **kwargs, append=False, logging=False)
+        return new_obj
+
+
+class VariableConfiguration(object):
+    def __init__(self, **kwargs):
+        self.name = kwargs.pop('name', '')
+        self.set_attr(**kwargs)
+
+    def set_attr(self, **kwargs):
+        pyclass.set_object_attributes(self, **kwargs, append=True, logging=False)
+
+    def replace(self, **kwargs):
+        new_obj = VariableConfiguration()
+        new_obj.__dict__.update(self.__dict__)
+        pyclass.set_object_attributes(new_obj, **kwargs, append=False, logging=False)
+        return new_obj
+
 
 
 
