5710377880a63516180a6d1120bcacfb56e173c5
Lei Cai <joulecai@gmail.com>
2021-08-06 07:08:57 +0300

    Set the default file directory as ~/Geospacelab_Data

--- geospacelab/config/_preferences.py
+++ geospacelab/config/_preferences.py
@@ -1,3 +1,5 @@
+
+
 import pathlib
 
 # datahub_data_root_dir = pathlib.Path("/Users/lcai/01-Work/00-Data/")
@@ -8,7 +10,7 @@ class Preferences(object):
 
     def __init__(self):
         self.package_name = "geospacelab"
-        self.datahub_data_root_dir = pathlib.Path().cwd()
+        self.datahub_data_root_dir = None
 
     @property
     def datahub_data_root_dir(self):
@@ -16,6 +18,10 @@ class Preferences(object):
 
     @datahub_data_root_dir.setter
     def datahub_data_root_dir(self, path):
-        self._datahub_data_root_dir = pathlib.Path(path)
+        if path is None:
+            self._datahub_data_root_dir = pathlib.Path.home() / 'Geospacelab_Data'
+        else:
+            self._datahub_data_root_dir = pathlib.Path(path)
+
 
 preferences = Preferences()
