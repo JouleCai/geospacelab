2f4e61b86b8ab5bf5de64d0cfaa289b0b3172cc4
Lei Cai <lei.cai@oulu.fi>
2021-08-08 00:03:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setuptools.setup(
         'Intended Audience :: Developers',      # Define that your audience are developers
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',   # Again, pick a license
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         ],
