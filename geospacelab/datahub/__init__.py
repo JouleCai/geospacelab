00279ee0d392347b3d2c6e69c3287c6b0745fa27
Lei Cai <lei.cai@oulu.fi>
2021-09-03 13:12:06 +0300

    add docstrings

--- geospacelab/datahub/__init__.py
+++ geospacelab/datahub/__init__.py
@@ -36,7 +36,31 @@ def example():
 
 
 class DataHub(object):
+    """The core of the data manager in GeospaceLab to dock different datasets.
+
+    :param dt_fr: starting time.
+    :type dt_fr: datetime.datetime
+    :param dt_to: stopping time.
+    :type dt_to: datetime.datetime
+    :param visual: decide if a Visual object will be added to the variable objects. Options:['off'], 'on'.
+    :type visual: str
+    :param datasets: A dictionary that stores the datasets docked or added to the datahub. \
+The keys (indices) are the integer numbers starting from 1.
+    :type datasets: dict
+    :param variables: A dictionary that stores the variables assigned from their own datasets to the datahub. \
+Typically used for the viewers or outputs.
+    :type variables: dict
+
+    :example:
+
+    """
     def __init__(self, dt_fr=None, dt_to=None, visual='off', **kwargs):
+        """
+        :param dt_fr: starting time, see above
+        :param dt_to: stopping time, see above
+        :param visual: visual attribute, see above
+        :param kwargs: other keywords, used for class inheritance.
+        """
         self.dt_fr = dt_fr
         self.dt_to = dt_to
         self.visual = visual
@@ -46,6 +70,20 @@ class DataHub(object):
         super().__init__(**kwargs)
 
     def dock(self, datasource_contents, **kwargs):
+        """To dock a sourced dataset.
+
+        :param datasource_contents: the contents that required for docking a sourced dataset. \
+To look up the sourced dataset and the associated contents, call :func:`~__init__.DataHub.list_sourced_datasets()`.
+        :type datasource_contents: list
+        :param dt_fr: starting time, optional, use datahub.dt_fr if not specified.
+        :type dt_fr: datetime.datetime
+        :param dt_to: stopping time, optional, use datahub.dt_to if not specified.
+        :type dt_to: datetime.datetime
+        :param visual: variable attribute, use datahub.visual if not specified.
+        :type visual: str
+        :return: a dataset
+        :rtype: Dataset object
+        """
         kwargs.setdefault('dt_fr', self.dt_fr)
         kwargs.setdefault('dt_to', self.dt_to)
         kwargs.setdefault('visual', self.visual)
@@ -74,10 +112,23 @@ class DataHub(object):
         return dataset
 
     def add_dataset(self, *args, kind='temporary', **kwargs):
+        """Add one or multiple datasets, which is not sourced in the package.
+        :param args: a list of the datasets.
+        :param kind: the kind of a dataset, options: ['temporary'], or 'user-defined'. \
+if temporary, a new dataset will be created from the DatasetModel.
+        :param kwargs: other keywords
+        :return: dataset or None
+        """
         kwargs.setdefault('dt_fr', self.dt_fr)
         kwargs.setdefault('dt_to', self.dt_to)
         kwargs.setdefault('visual', self.visual)
         kwargs.setdefault('datasets', [])
+
+        if kind == 'temporary':
+            kwargs.pop('datasets', None)
+            dataset = DatasetModel(name='temporary', **kwargs)
+            self._append_dataset(dataset)
+            return dataset
 
         for dataset in args:
             kwargs['datasets'].append(dataset)
@@ -90,12 +141,14 @@ class DataHub(object):
             else:
                 TypeError('A dataset instance\'s class must be a heritage of the class DatasetModel!')
 
-        if kind == 'temporary':
-            dataset = DatasetModel(name='temporary', **kwargs)
-            self._append_dataset(dataset)
-            return dataset
+        return
 
     def _append_dataset(self, dataset):
+        """Append a dataset to the datahub
+
+        :param dataset: a dataset (Dataset object)
+        :return: None
+        """
         ind = len(self.datasets.keys()) + 1
         name = 'dataset_{:02d}'.format(ind)
         if dataset.name is None:
@@ -104,6 +157,15 @@ class DataHub(object):
         self._latest_dataset_ind = ind
 
     def get_variable(self, var_name, dataset=None, dataset_index=None):
+        """To get a variable from the docked or added dataset.
+
+        :param var_name: the name of the queried variable
+        :param dataset: the dataset storing the queried variable.
+        :param dataset_index: the index of the dataset in datahub.datasets. \
+if both dataset or dataset_index are not specified, the function will get the \
+variable from the assigned variables.
+        :return: None or the queried variable (A VariableModel or its subclass object)
+        """
         if dataset is None and dataset_index is None:
             var = None
             for ind, var in self.variables.items():
@@ -121,11 +183,22 @@ class DataHub(object):
         if dataset.exist(var_name):
             var = dataset[var_name]
         else:
+            mylog.StreamLogger.warning(
+                f"Cannot find the queried variable in {repr(dataset)}. Return None.")
             var = None
 
         return var
 
     def assign_variable(self, var_name, dataset=None, dataset_index=None, add_new=False, **kwargs):
+        """Assign a variable from the docked or added dataset.
+
+        :param var_name: The name of the variable
+        :param dataset: The dataset that stores the variable
+        :param dataset_index: The index of the dataset in the datahub.datasets.
+        :param add_new: if True, add the variable to the specified dataset and assign to the datahub
+        :param kwargs: other keywords to configure the attributes of the variable.
+        :return: the variable.
+        """
         if dataset is None:
             if dataset_index is None:
                 dataset = self.datasets[self._latest_dataset_ind]  # the latest added dataset
@@ -149,11 +222,20 @@ class DataHub(object):
         return var
 
     def _append_variable(self, var):
+        """ append a variable to datahub.variables
+
+        :param var: a variable object
+        :return: None
+        """
         ind = len(self.variables.keys()) + 1
         self.variables[ind] = var
 
     @staticmethod
     def list_sourced_datasets():
+        """To list all the sourced datasets bult-in this package
+
+        The list will be printed in the python console in a \"tree\" view.
+        """
         this_file_dir = pathlib.Path(__file__).resolve().parent
         data_source_dir = this_file_dir / 'sources'
         sub_dirs = list(data_source_dir.glob("**"))
@@ -193,8 +275,11 @@ class DataHub(object):
 
         pybasic.dict_print_tree(data_sources, full_value=False, dict_repr=False, value_repr=True, max_level=None)
 
-
     def list_datasets(self):
+        """ To list all the datasets that have been docked or added to the datahub
+
+        The list will be printed in the console as a table
+        """
         # print header
         mylog.simpleinfo.info("Listing datasets ...")
         mylog.simpleinfo.info('{:^20s}{:60s}'.format('Index', 'Dataset'))
@@ -204,6 +289,10 @@ class DataHub(object):
         print()
 
     def list_assigned_variables(self):
+        """ To list all the assigned variables that have been docked or added to the datahub
+
+        The list will be printed in the console as a table
+        """
         # print header
         mylog.simpleinfo.info("Listing the assigned variables ...")
 
@@ -223,6 +312,7 @@ class DataHub(object):
     def __repr__(self):
         rep = f"GeospaceLab DataHub object <starting time: {str(self.dt_fr)} and stopping time: {str(self.dt_to)}>"
         return rep
+
 
 if __name__ == "__main__":
     example()
