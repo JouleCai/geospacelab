adec9318c49cfdf7ca60a5a483bbcd15bbd8518e
Lei Cai <joulecai@gmail.com>
2021-10-18 19:13:41 +0300

    add plotly

---
+++ geospacelab/visualization/plotly/ipanel.py
@@ -0,0 +1,19 @@
+
+
+class Panel(object):
+    def __init__(self):
+        pass
+
+    def add_line(self):
+        pass
+
+    def add_image(self):
+        pass
+
+    def add_pcolor(self):
+        pass
+
+    def add_scatter(self):
+        pass
+
+    
